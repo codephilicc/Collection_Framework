651cb36b306bf7a1fd7f10bbbf4835a3a72f3448
Rahul <rahulkr.vk@gmail.com>
2024-01-06 18:40:06 +0530

    add keyset method in hashmap

--- src/com/Collections/map/hashmap.java
+++ src/com/Collections/map/hashmap.java
@@ -1,6 +1,7 @@
 package com.Collections.map;
 
 import java.util.HashMap;
+import java.util.Set;
 
 public class hashmap {
 
@@ -27,17 +28,28 @@ public class hashmap {
 			String s = map.get(8);
 			System.out.println("value for key " + "\"8\" is: " + s);
 		}
+//		use keyset method which result set and from that we can extract the value
+		Set<Integer> set = map.keySet();
+		for(int i : set) {
+			System.out.println("The value of "+i+ " key is "+map.get(i));
+		}
 
 	}
 
 }
 
 /*
- 4
+4
 vivek
 false
 {0=aman, 2=vivek, 5=bob, 7=aman, 8=aman}
 value for key "8" is: aman
+The value of 0 key is aman
+The value of 2 key is vivek
+The value of 5 key is bob
+The value of 7 key is aman
+The value of 8 key is aman
+
 
  */
 
