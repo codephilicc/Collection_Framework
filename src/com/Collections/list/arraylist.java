22f2b5dcb1bb5718bc71ad6a460ba1fa6f662fcf
Rahul <rahulkr.vk@gmail.com>
2024-01-06 10:46:33 +0530

    arralist iterator

--- src/com/Collections/list/arraylist.java
+++ src/com/Collections/list/arraylist.java
@@ -1,6 +1,7 @@
 package com.Collections.list;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class arraylist {
@@ -13,9 +14,15 @@ public class arraylist {
 		ll.add("kabaddi");
 		ll.add("crikcet");
 		ll.add("hockey");
-		
-		ll.forEach(x->System.out.println("games are: "+x));
-		
+
+		ll.forEach(x -> System.out.println("games are: " + x));
+
+		Iterator<String> itr = ll.iterator();
+
+		while (itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+
 	}
 
 }
