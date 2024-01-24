987e50046faa93f2f0520bdcc6855406a989b39a
Rahul <rahulkr.vk@gmail.com>
2024-01-24 22:20:16 +0530

    stream1

--- src/com/Collections/stream/streamApi.java
+++ src/com/Collections/stream/streamApi.java
@@ -1,10 +1,30 @@
 package com.Collections.stream;
 
+import java.util.*;
+
 public class streamApi {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+//		
+//		List number = Arrays.asList(2,3,4,5);
+//		List square = number.stream().map(x->x*x).collect(Collectors.toList());
 
+//		List names = Arrays.asList("Reflection","Collection","Stream");
+//		List result = names.stream().filter(s->s.startsWith("S")).collect(Collectors.toList());
+////	
+//		List names = Arrays.asList("Reflection","Collection","Stream");
+//		List result = names.stream().sorted().collect(Collectors.toList());
+//	
+//		List number = Arrays.asList(2,3,4,5,3);
+//		Set square = number.stream().map(x->x*x).collect(Collectors.toSet());
+//		
+//		List number = Arrays.asList(2,3,4,5);
+//		number.stream().map(x->x*x).forEach(y->System.out.println(y));
+		
+		
 	}
+	
+	
 
 }
