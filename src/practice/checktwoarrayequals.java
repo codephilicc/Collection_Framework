7d265f6c92255092a9dcdc69b38c84c586090a97
Rahul <rahulkr.vk@gmail.com>
2024-02-14 07:30:41 +0530

    merge issue

---
+++ src/practice/checktwoarrayequals.java
@@ -0,0 +1,19 @@
+//package practice;
+//
+//import java.util.*;
+//
+//public class checktwoarrayequals {
+//
+//	public static void main(String[] args) {
+//
+//		int[] arr1 = new int[]{ 2, 4, 1, 5 };
+//		int[] arr2 = new int[]{ 2, 4, 1, 5 };
+//
+////		System.out.println("check arrays equals or not: " + arr1.equals(arr2));
+//		System.out.println("is arr1 equals to arr2 : " +
+//                Arrays.equals(arr1, arr2));
+//		
+//		
+//	}
+//
+//}
